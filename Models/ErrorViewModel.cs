5f970092c20b5acb8006eca7ad0c242218c25991
fernandoprincipecuellar <fernandoprin20.a@gmail.com>
2026-04-19 13:58:36 -0500

    inicio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Practica2_Teoria.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
